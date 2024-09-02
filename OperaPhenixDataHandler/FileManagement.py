79e6d934ca995a1fb882d531ef55745c5b571f21
RossScrim <70106868+RossScrim@users.noreply.github.com>
2024-09-02 16:50:13 +0100

    Finished making FilePathHandler for generating paths to load config and keys data paths and adjusted the ConfigReader to convert txt file to JSON with the added feature of skipping lines of that are headers and footersSTill a JSONReader is still there if the reading from .JSON and not .txt

--- OperaPhenixDataHandler/FileManagement.py
+++ OperaPhenixDataHandler/FileManagement.py
@@ -1,64 +1,43 @@
 import os
 import re
-import ConfigReader
 
 
-def create_dir(save_path):
-    if not os.path.exists(save_path):
+class FilePathHandler:
+    def __init__(self, archived_data_path: str):
+        self.archived_data_path = archived_data_path + "\\"
+        self.archived_data_config = os.path.join(self.archived_data_path,
+                                                 self.get_name_from_regexstring(self.archived_data_path, ".*\.kw\.txt")[0])
+        self.archived_image_path = self.archived_data_path + "\\images"
+        self.well_names = self.get_name_from_regexstring(self.archived_image_path, 'r(\d+)c(\d+)')
+
+    def get_name_from_regexstring(self, dir_path: str, str_pattern: str):
+        matched_string = [match.group() for file_name in os.listdir(dir_path) if
+                                 (match := re.search(str_pattern, file_name))]
+        return matched_string
+
+    # DO we need the below function in class or should we grab the images to process when needed
+    def get_opera_phenix_images_from_FOV(self, well_name: str, pattern):
+        well_path = os.path.join(self.archived_image_path, well_name)
+        image_files = self.get_name_from_regexstring(well_path, pattern)
+        return image_files
+
+    def create_dir(save_path):
+        if not os.path.exists(save_path):
         os.makedirs(save_path)
 
 
-class FilePathCreator:
-    def __init__(self, archived_data_path: str):
-        self.archived_data_path = archived_data_path
-
-        self.archived_image_path = self.archived_data_path + "\\images" # maybe shhold be a function rather than creating muliple paths that are useless
-        self.well_names = self.get_well_names(self.archived_image_path)
-
-        if not self.config_file["timelapse"]:
-            self.timelapse_image_paths = self.build_timelapse_image_filename()
-
-        else:
-            self.image_paths =  self.build_image_filename()
-
-    def get_well_names(self, dir_path: str):
-        pattern = 'r(\d+)c(\d+)'
-        unique_matched_wells = {match.group() for file_name in os.listdir(dir_path) if
-                                (match := re.search(pattern, file_name))}
-        return sorted(unique_matched_wells)
-
-    def build_timelapse_image_filename(self, dir_path: str, well_names: list, field_name: str, planes: int, channels: int,
-                                       timepoints: int):
-        image_paths = []
-        for well_name in well_names:
-            well_path = os.path.join(dir_path, well_name)
-            for channel in range(1, channels + 1):
-                for time_point in range(1, timepoints + 1):
-                    for plane in range(1, planes + 1):
-                        filename = f"{well_name}{field_name}p{plane:02d}-ch{channel:02d}t{time_point:02d}.tiff"
-                        image_path = os.path.join(well_path, filename)
-                        if os.path.isfile(image_path):
-                            image_paths.append(filename)
-                        else:
-                            print(f'{filename} does not exist')
-        return image_paths
-
-    def build_image_filename(self, dir_path: str, well_names: list, field_name: str, planes: int, channels: int):
-        image_paths = []
-        for well_name in well_names:
-            well_path = os.path.join(dir_path, well_name)
-            for channel in range(1, channels + 1):
-                for plane in range(1, planes + 1):
-                    filename = f"{well_name}{field_name}p{plane:02d}-ch{channel:02d}t01.tiff"
-                    image_path = os.path.join(dir_path,filename)
-                    if os.path.isfile(image_path):
-                        image_paths.append(filename)
-                    else:
-                        print(f'{filename} does not exist')
-        return image_paths
-
-
 if __name__ == "__main__":
-    config = ConfigReader.ConfigReader("../config.json")
-    config_file = config.get_config()
-    print(FilePathCreator(config_file).well_paths)
+    archived_data_path = r"X:\Ross\20241705_RPE1P53KO_siMMR_live-cell\hs\88651da0-9ab0-4728-816f-15417fe0fa61"
+    files = FilePathHandler(archived_data_path)
+
+
+    field_of_view = 4
+    for field in range(0, field_of_view):
+        pattern = f"r\d+c\d+f0{field}p\d+-ch\d+t\d+.tiff"
+        print(pattern)
+        print(files.get_opera_phenix_images_from_FOV(files.well_names[0], pattern))
+        ## do some processing
+
+    print(files.archived_data_path)
+    print(files.archived_data_config)
+    print(files.well_names)
