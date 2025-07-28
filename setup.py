0b1ed2e3f5663f7807cc07201fa4dd5d0ca0edbf
RossScrim <70106868+RossScrim@users.noreply.github.com>
2025-07-28 15:38:17 +0100

    Rename OperaPhenixDataHandler to HiConA and update setup
    All source files and resources were moved from OperaPhenixDataHandler to a new HiConA package. The setup.py and related metadata were updated to reflect the new package name, authors, and repository URL. .gitignore and project configuration were also updated, and obsolete __pycache__ files were removed.

--- setup.py
+++ setup.py
@@ -7,19 +7,19 @@ with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='Opera_Data_Processor',
+    name='HiConA',
     version='1.1.0',
     author=['Ross Scrimgeour', 'Emma Westlund'],
-    author_email='your@email.com',
+    author_email=['rcscrimgeour@gmail.com', 'ross.scrimgeour@icr.ac.uk', 'emma.westlund@icr.ac.uk'],
     description='Short description of your package',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/your_project',
+    url='https://github.com/RossScrim/HiConA',
     packages=find_packages(),
     install_requires=requirements,
     entry_points={
         'console_scripts': [
-            'Opera_Processor = OperaPhenixDataHandler.main:main',
+            'HiConA.run = HiConA.main:main',
         ],
     },
     classifiers=[
