8e5e26e063d48857fd7189620c27e7636415909e
RossScrim <70106868+RossScrim@users.noreply.github.com>
2025-11-18 15:19:06 +0000

    Refactor file structure and update imports for HiConA modules

---
+++ HiConA/Utilities/IOread.py
@@ -0,0 +1,12 @@
+import os
+import tifffile
+import numpy as np
+
+def load_images(filepaths):
+    im_arr = np.array([tifffile.imread(fp) for fp in filepaths])
+    return im_arr
+
+def save_images(full_file_path, images, axes_order="YX"):
+    tifffile.imwrite(full_file_path,
+                     images,
+                     imagej=True, metadata={'axes': f'{axes_order}'})
