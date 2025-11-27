d3e6a584d2dffac2297960ba664dab6e886c2793
ewestlundicr <emma.westlund@icr.ac.uk>
2025-11-27 16:39:23 +0000

    Fixed error with TileConfiguration file

---
+++ HiConA/Backend/HiConAStitching.py
@@ -0,0 +1,15 @@
+import imagej
+import os
+import re
+import scyjava
+import tkinter as tk
+import tkinter.ttk as ttk
+from tkinter import messagebox
+from tkinter.filedialog import askdirectory
+from shutil import copy
+
+class HiConAStitching:
+    def __init__(self):
+        pass
+
+    
