88aa3b219a0534a71853067ae797f13111ec30e3
RossScrim <70106868+RossScrim@users.noreply.github.com>
2026-02-07 22:53:14 +0000

    Clean up main.py by removing unused comments
    Removed commented-out code related to config file processing.

--- HiConA/main.py
+++ HiConA/main.py
@@ -2,8 +2,6 @@ import ttkbootstrap as tb
 
 from HiConA.Backend.HiConAWorkFlowHandler import HiConAWorkflowHandler
 from HiConA.Backend.ImageJ_singleton import ImageJSingleton
-from Utilities.ConfigReader import ConfigReader
-
 from HiConA.GUI.GUI_HiConA import HiConAGUI
 
 
@@ -19,13 +17,6 @@ def main():
     print("Processing started!")
 
     for measurement_id in all_files.keys():
-        #config_file = ConfigReader(all_files[measurement_id].archived_data_config).load(remove_first_lines=1, remove_last_lines=2)
-        #if config_file is not None:
-        #    print(config_file["PLATENAME"])
-
-        #print("Processing measurement ID:", measurement_id)
-        #print(all_files[measurement_id])
-        #print(processes)
         HiConAWorkflowHandler(all_xml_readers[measurement_id], all_files[measurement_id], processes, output_dir).run()
 
     print("Processing finished!")
